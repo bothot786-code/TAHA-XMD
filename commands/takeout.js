8a297e7d32c6811ccf51e7bd216dd795545969a3
bothot786-code <bothot786@gmail.com>
2026-07-05 01:37:09 +0500

    Update takeout.js

--- commands/takeout.js
+++ commands/takeout.js
@@ -15,8 +15,8 @@ const channelInfo = {
         forwardingScore: 1,
         isForwarded: true,
         forwardedNewsletterMessageInfo: {
-            newsletterJid: '120363406588763460@newsletter',
-            newsletterName: 'Gᴀᴀᴊᴜ-Xᴍᴅ',
+            newsletterJid: '120363409838427367@newsletter',
+            newsletterName: 'Taha-Xᴍᴅ',
             serverMessageId: -1
         }
     }
