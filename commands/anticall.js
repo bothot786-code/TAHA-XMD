4236c5893f9f0a91a950bf09f061c439b5ca82e1
bothot786-code <bothot786@gmail.com>
2026-07-04 23:25:17 +0500

    Update anticall.js

--- commands/anticall.js
+++ commands/anticall.js
@@ -35,8 +35,8 @@ const channelInfo = {
         forwardingScore: 1,
         isForwarded: true,
         forwardedNewsletterMessageInfo: {
-            newsletterJid: '120363406588763460@newsletter',
-            newsletterName: 'Gᴀᴀᴊᴜ-Xᴍᴅ',
+            newsletterJid: '120363409838427367@newsletter',
+            newsletterName: 'TAHA MD',
             serverMessageId: -1
         }
     }
