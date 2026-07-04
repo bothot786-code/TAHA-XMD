3cd70b319334df518500148eb09f2500893c772e
bothot786-code <bothot786@gmail.com>
2026-07-05 01:20:24 +0500

    Update getjid.js

--- commands/getjid.js
+++ commands/getjid.js
@@ -24,8 +24,8 @@ async function getjidCommand(sock, chatId, message) {
                 forwardingScore: 1,
                 isForwarded: true,
                 forwardedNewsletterMessageInfo: {
-                    newsletterJid: '120363406588763460@newsletter',
-                    newsletterName: 'Gᴀᴀᴊᴜ-Xᴍᴅ',
+                    newsletterJid: '120363409838427367@newsletter',
+                    newsletterName: 'Taha-Xᴍᴅ',
                     serverMessageId: -1
                 }
             }
